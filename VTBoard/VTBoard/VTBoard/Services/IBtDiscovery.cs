cac946d4c7ad38918b33d971244aca5df583e8a4
markftwerner <35277196+markftwerner@users.noreply.github.com>
2018-01-30 18:59:03 -0700

    All is good

---
+++ VTBoard/VTBoard/VTBoard/Services/IBtDiscovery.cs
@@ -0,0 +1,18 @@
+﻿using VTBoard.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTBoard.Services
+{
+    public interface IBtDiscovery : INotifyPropertyChanged
+    {
+        bool IsBusy { get; }
+        List<BtDeviceInfo> PairedDevices { get; }
+
+        void Refresh();
+    }
+}
