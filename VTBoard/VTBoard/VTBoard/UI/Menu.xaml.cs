f819a8af2c3767411534008915ebd7651a38a9b9
Jdidde <jdidde11@jdiddes-air.mobile.asu.edu>
2018-02-26 16:06:22 -0700

    update

---
+++ VTBoard/VTBoard/VTBoard/UI/Menu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VTBoard
+{
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VTBoard/VTBoard/VTBoard/UI/MenuViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VTBoard
+{
+    public class MenuViewModel
+    {
+        public MenuViewModel()
+        {
+        }
+    }
+}
