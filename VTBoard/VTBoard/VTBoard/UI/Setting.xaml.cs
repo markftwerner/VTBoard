7a9a3bcab391f7f335b2e6941344e117600e4e32
markftwerner <35277196+markftwerner@users.noreply.github.com>
2018-02-26 15:42:46 -0700

    Cohesion Setting

--- VTBoard/VTBoard/VTBoard/UI/Setting.xaml.cs
+++ VTBoard/VTBoard/VTBoard/UI/Setting.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-namespace VTBoard
+namespace VTBoard.UI
 {
     public partial class Setting : ContentPage
     {
         public Setting()
         {
             InitializeComponent();
+            BindingContext = new Setting_ViewModel();
         }
     }
 }
