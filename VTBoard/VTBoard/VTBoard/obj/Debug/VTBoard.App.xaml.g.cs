6bfd8cf07f7e8d396685b642219847fbf45cda76
Jdidde <jdidde11@jdiddes-air.mobile.asu.edu>
2018-02-26 15:35:25 -0700

    Update

--- VTBoard/VTBoard/VTBoard/obj/Debug/VTBoard.App.xaml.g.cs
+++ VTBoard/VTBoard/VTBoard/obj/Debug/VTBoard.App.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace VTBoard {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Mark\\source\\repos\\VTBoard\\VTBoard\\VTBoard\\VTBoard\\App.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/Users/jdidde11/Desktop/VTBoard/VTBoard/VTBoard/VTBoard/App.xaml")]
     public partial class App : global::Xamarin.Forms.Application {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
