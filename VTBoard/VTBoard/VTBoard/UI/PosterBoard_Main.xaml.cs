eac977130855c3f1256435f0ebfcc44b6ece7a16
markftwerner <35277196+markftwerner@users.noreply.github.com>
2018-02-26 15:09:39 -0700

    More cohesion

--- VTBoard/VTBoard/VTBoard/UI/PosterBoard_Main.xaml.cs
+++ VTBoard/VTBoard/VTBoard/UI/PosterBoard_Main.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-namespace VTBoard
+namespace VTBoard.UI
 {
     public partial class PosterBoard_Main : ContentPage
     {
         public PosterBoard_Main()
         {
+
             InitializeComponent();
+            BindingContext = new PosterBoard_MainViewModel();
+        
         }
     }
 }
