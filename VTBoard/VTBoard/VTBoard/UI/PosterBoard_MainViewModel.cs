1375ec3f721d193efa12d7b27986657318b167ed
markftwerner <35277196+markftwerner@users.noreply.github.com>
2018-02-26 15:19:39 -0700

    Cohesion

--- VTBoard/VTBoard/VTBoard/UI/PosterBoard_MainViewModel.cs
+++ VTBoard/VTBoard/VTBoard/UI/PosterBoard_MainViewModel.cs
@@ -1,7 +1,22 @@
 ﻿using System;
-namespace VTBoard
+using VTBoard.Model;
+using VTBoard.Services;
+using ReactiveUI;
+using Splat;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using System.Reactive;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace VTBoard.UI
 {
-    public class PosterBoard_MainViewModel
+    public class PosterBoard_MainViewModel : ReactiveObject
     {
         public PosterBoard_MainViewModel()
         {
