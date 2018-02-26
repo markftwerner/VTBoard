ed9b0d7ad99eb46901c7c0214c0f898b9eab769f
Jdidde <jdidde11@jdiddes-air.mobile.asu.edu>
2018-02-26 15:39:27 -0700

    wow

---
+++ VTBoard/VTBoard/VTBoard/UI/Setting.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VTBoard
+{
+    public partial class Setting : ContentPage
+    {
+        public Setting()
+        {
+            InitializeComponent();
+        }
+    }
+}
