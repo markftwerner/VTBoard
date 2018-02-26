53b03af7ee1c408cbf4819ab5929550a69d4c602
markftwerner <35277196+markftwerner@users.noreply.github.com>
2018-02-26 15:06:51 -0700

    Edits to cohesion

--- VTBoard/VTBoard/VTBoard/UI/Network_Domain.xaml.cs
+++ VTBoard/VTBoard/VTBoard/UI/Network_Domain.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-namespace VTBoard
+namespace VTBoard.UI
 {
     public partial class Network_Domain : ContentPage
     {
         public Network_Domain()
         {
             InitializeComponent();
+            BindingContext = new Network_DomainViewModel();
         }
     }
 }
